6bbdf6d9beaef24b13cce81bbdf438322941fc4b
Svilen Todorov <svilen@aristander.ai>
2020-02-07 19:50:32 +0100

    Add Link Previews

--- _scripts/push.sh
+++ _scripts/push.sh
@@ -1,9 +1,9 @@
 JEKYLL_ENV=production jekyll build
-python3.7 -m http.server -d ../_site/
+python3.7 -m http.server -d ../_site/ &
 python3.7 add_images.py
 cd ..
 cp -rn _site/static/previews/* static/previews/
-git add .
+git add .	
 git commit -m "$1"
 git push
 cp -ru _site/* ../Tenoke.github.io/
@@ -12,3 +12,4 @@ git add .
 git commit -m "$1"
 git push 
 cd ../jekyll-blog
+fg
